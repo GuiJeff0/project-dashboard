5bc87dbb916d7a73b01662bb9ca69b7bc3c3f35f
Guilherme Santos <guilhermejeffofc@gmail.com>
2024-05-28 23:15:57 -0300

    Project created

---
+++ backend/src/test/java/com/projectweb/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectweb.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
